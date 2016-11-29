79dd4a6fe6c2dc4f9b070e5108016e833ad3bac5
Paweł Jaruga <helferth95@gmail.com>
2016-11-29 16:11:39 +0100

    [Refactor] Cykl 2
    Zmiana struktóry testów. Dodano @BeforeMethod.
    
    Testy nadal przechodzą pomyślnie.

--- test/rrs/RaceResultsServiceTest.java
+++ test/rrs/RaceResultsServiceTest.java
@@ -1,29 +1,31 @@
 package rrs;
 
-import static org.testng.Assert.*;
 import org.testng.annotations.*;
 import static org.mockito.Mockito.*;
 
 @Test
 public class RaceResultsServiceTest {
+    private RaceResultsService raceResults;
+    private Message message;
+    private Client clientA;
+    private Client clientB;
+    
+    @BeforeMethod
+    public void setUp() {
+        raceResults = new RaceResultsService();
+        clientA = mock(Client.class);
+        clientB = mock(Client.class);
+        message = mock(Message.class);
+    }
     
     public void subscribedClientShouldReceiveMessage() {
-        RaceResultsService raceResults = new RaceResultsService();
-        Client client = mock(Client.class);
-        Message message = mock(Message.class);
-        
-        raceResults.addSubscriber(client);
+        raceResults.addSubscriber(clientA);
         raceResults.send(message);
         
-        verify(client).receive(message);
+        verify(clientA).receive(message);
     }
     
     public void messageShouldBeSendToAllSubscribedClients() {
-        RaceResultsService raceResults = new RaceResultsService();
-        Client clientA = mock(Client.class);
-        Client clientB = mock(Client.class);
-        Message message = mock(Message.class);
-        
         raceResults.addSubscriber(clientA);
         raceResults.addSubscriber(clientB);
         raceResults.send(message);
