3e3984b3111a3d34f3b687dcdba346253d966901
Paweł Jaruga <helferth95@gmail.com>
2016-11-29 16:09:15 +0100

    [Implement] Cykl 2
    Dodano ArrayList Klientów oraz zmodyfikowano metodę send tak, aby każdy
    klient otrzymywał wiadomość.
    
    Oba testy przechodzą pomyślnie.

--- src/rrs/RaceResultsService.java
+++ src/rrs/RaceResultsService.java
@@ -1,12 +1,17 @@
 package rrs;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class RaceResultsService {
-    private Client client; 
+    private Collection<Client> clients = new ArrayList<Client>();
     
     public void addSubscriber(Client client) {
-        this.client = client;
+        clients.add(client);
     }
     public void send(Message message) {
-        client.receive(message);
+        for( Client client:clients) {
+            client.receive(message);
+        }
     }
 }
