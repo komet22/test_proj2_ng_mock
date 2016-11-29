16be863f55d2d1e838b878f5c98fefb961a07a80
Paweł Jaruga <helferth95@gmail.com>
2016-11-29 16:01:01 +0100

    [IMPLEMENT] Cykl 1
    Dodane metody addSubscriber i send.
    
    Test przechodzi pomyślnie.

--- src/rrs/RaceResultsService.java
+++ src/rrs/RaceResultsService.java
@@ -1,11 +1,12 @@
 package rrs;
 
 public class RaceResultsService {
-        
+    private Client client; 
+    
     public void addSubscriber(Client client) {
-        
+        this.client = client;
     }
     public void send(Message message) {
-        
+        client.receive(message);
     }
 }
